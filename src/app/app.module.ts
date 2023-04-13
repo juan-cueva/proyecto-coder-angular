52c5a5c34421625a25e8f202d675c31d36ef4514
Juan Cueva <juancuevac97@gmail.com>
2023-04-13 15:31:58 -0500

    feat: Crear componentes adicionales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { StudentsComponent } from './students/students.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    NavbarComponent,
+    StudentsComponent
   ],
   imports: [
     BrowserModule
